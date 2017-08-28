918c7624fdd8ea867d7d80375383bc8da6bf94e7
Akshat Nigam <akshat.nigam@blockops.studio>
2017-08-28 18:51:22 +0530

    Guice Module created for SecP256K1

--- src/main/java/studio/blockops/vyom/core/crypto/secp256k1/SecP256K1Module.java
+++ src/main/java/studio/blockops/vyom/core/crypto/secp256k1/SecP256K1Module.java
@@ -7,7 +7,6 @@ import org.bouncycastle.crypto.params.ECDomainParameters;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import com.google.inject.assistedinject.FactoryModuleBuilder;
 
 import studio.blockops.vyom.core.crypto.Curve;
 
