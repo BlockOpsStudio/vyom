e2119032e29626da06f5430daebb074962a7f247
Akshat Nigam <akshat.nigam@blockops.studio>
2017-09-03 15:30:41 +0530

    SecP256K1Module defined with all SecP256K1 bindings.

--- src/main/java/studio/blockops/vyom/core/crypto/secp256k1/SecP256K1Module.java
+++ src/main/java/studio/blockops/vyom/core/crypto/secp256k1/SecP256K1Module.java
@@ -7,14 +7,25 @@ import org.bouncycastle.crypto.params.ECDomainParameters;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
 
+import studio.blockops.vyom.core.crypto.BlockCipher;
+import studio.blockops.vyom.core.crypto.CryptoEngine;
 import studio.blockops.vyom.core.crypto.Curve;
+import studio.blockops.vyom.core.crypto.KeyAnalyzer;
+import studio.blockops.vyom.core.crypto.KeyGenerator;
+import studio.blockops.vyom.core.crypto.Signer;
 
 public class SecP256K1Module extends AbstractModule {
 	
 	@Override
 	protected void configure() {
-		
+		install(new FactoryModuleBuilder()
+			.implement(Signer.class, SecP256K1Signer.class)
+			.implement(KeyGenerator.class, SecP256K1KeyGenerator.class)
+			.implement(BlockCipher.class, SecP256K1BlockCipher.class)
+			.implement(KeyAnalyzer.class, SecP256K1KeyAnalyzer.class)
+			.build(CryptoEngine.class));
 	}
 	
 	@Provides @Singleton
