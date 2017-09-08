582103f4b6876306f066f1bf9d5c18b05ac0bd99
Akshat Nigam <akshat.nigam@blockops.studio>
2017-09-08 08:21:41 +0530

    SecP256K1Signer verify tests created.

--- src/test/java/studio/blockops/vyom/core/crypto/secp256k1/SecP256K1SignerTest.java
+++ src/test/java/studio/blockops/vyom/core/crypto/secp256k1/SecP256K1SignerTest.java
@@ -2,6 +2,8 @@ package studio.blockops.vyom.core.crypto.secp256k1;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.math.BigInteger;
 
@@ -61,5 +63,52 @@ public class SecP256K1SignerTest {
 			
 			assertThat(signature.toString(), equalTo(expectedSignature));
 		}
+	}
+	
+	@RunWith(JukitoRunner.class)
+	public static class VerifyTest extends SecP256K1SignerTest {
+
+		public static class Module extends JukitoModule {
+
+			@Override
+			protected void configureTest() {
+				install(new CryptoModule());
+				install(new SecP256K1Module());
+			}
+		}
+
+	    @Test
+	    public void testVerifySignature1() {	        
+			final String exampleMessage = "This is an example of a signed message.";	
+			final byte[] input = exampleMessage.getBytes();
+			
+	        final BigInteger r = new BigInteger("95350169487015575001444507567851457309689354890536757640816472151471942911739");
+	        final BigInteger s = new BigInteger("53263359985948886716508128220321578640585687230908938312863706887350789467339");
+	        final Signature signature = new Signature(r, s, (byte) 28);
+			
+	        assertTrue(signer.verify(input, signature));
+	    }
+		
+		@Test
+		public void testVerifySignature2() {
+			final String exampleMessage = "This is an example of a signed message.";			
+			final byte[] input = exampleMessage.getBytes();
+			
+			final Signature signature = signer.sign(input);
+			
+	        assertTrue(signer.verify(input, signature));
+		}
+
+	    @Test
+	    public void testVerifySignature3() {	        
+			final String exampleMessage = "This is an example of a signed message.";	
+			final byte[] input = exampleMessage.getBytes();
+			
+	        final BigInteger r = new BigInteger("28157690258821599598544026901946453245423343069728565040002908283498585537001");
+	        final BigInteger s = new BigInteger("30212485197630673222315826773656074299979444367665131281281249560925428307087");
+	        final Signature signature = new Signature(r, s, (byte) 28);
+			
+	        assertFalse(signer.verify(input, signature));
+	    }
 	}
 }
